1a30626f059dc5ccdba2f34421e680e60f1825f5
Olha Sotova <sotovaola21@gmail.com>
2025-02-09 18:03:01 +0100

    technologies has fixed

--- components/sections/Technologies.tsx
+++ components/sections/Technologies.tsx
@@ -3,7 +3,7 @@
 import { motion } from "framer-motion";
 import { Container } from "@/components/Container";
 import { Tag } from "@/components/ui/tag";
-import { SectionHeader } from "../ui/SectionHeader";
+import ArrowRightButton from "../ui/arrowRightBtn";
 
 interface TechnologiesProps {
   logos: { src: string; alt: string }[];
@@ -21,19 +21,14 @@ export function Technologies({ logos }: TechnologiesProps) {
             transition={{ duration: 0.5 }}
             className="w-full md:w-1/3"
           >
-                   <SectionHeader
-                     title="Our technologies"
-                     tagText="Technologies"
-                     tagVariant="black"
-                     textVariant="black"
-                   />
-            <p className="text-lg mt-2">
-              Lorem ipsum dolor sit amet consectetur. Viverra est eget ut ipsum
-              vulputate adipiscing condimentum.
+            <Tag text="Our Technologies" variant="black" />
+            <h2 className="text-[40px] font-bold mt-4">Technologies</h2>
+            <p className="text-lg my-[30px]">
+              Lorem ipsum dolor sit amet consectetur. Viverra est eget ut ipsum vulputate adipiscing condimentum.
             </p>
-            <button className="mt-6 bg-btn_color hover:bg-blue-700 text-white px-6 py-3 rounded-full text-sm font-medium transition">
-              Learn more →
-            </button>
+            <div className="w-full ">
+              <ArrowRightButton text="Learn more" arrow={true} show={true} className="w-full" />
+            </div>
           </motion.div>
 
           <motion.div
@@ -41,38 +36,17 @@ export function Technologies({ logos }: TechnologiesProps) {
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.5 }}
-            className={`w-full flex justify-center ${
-              logos.length === 1
-                ? "flex justify-center items-center h-[200px]"
-                : logos.length === 2
-                ? "grid grid-cols-2 gap-12 justify-center"
-                : logos.length === 3
-                ? "grid grid-cols-3 gap-10 sm:grid-cols-2 sm:grid-rows-2 justify-center"
-                : logos.length === 4
-                ? "grid grid-cols-2 gap-12 justify-center"
-                : logos.length > 4 && logos.length <= 8
-                ? "grid grid-cols-4 gap-12 justify-center"
-                : "grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-4 gap-6 justify-center"
-            }`}
+            className="w-full grid grid-cols-2 gap-8 sm:grid-cols-4 lg:grid-cols-4 md:gap-6 justify-center"
           >
             {logos.map((logo, index) => (
-              <motion.img
-                key={index}
-                src={logo.src}
-                alt={logo.alt}
-                className={`mx-auto ${
-                  logos.length === 1
-                    ? "h-32 w-[360px]"
-                    : logos.length === 2
-                    ? "h-28 w-[220px]"
-                    : logos.length === 3
-                    ? "h-24 w-30"
-                    : logos.length === 4
-                    ? "h-24 w-30"
-                    : "h-16 w-30"
-                }`}
-                whileHover={{ scale: 1.1 }}
-              />
+              <motion.div key={index} className="flex justify-start">
+                <motion.img
+                  src={logo.src}
+                  alt={logo.alt}
+                  className="h-10 sm:h-14 md:h-16 lg:h-20 w-auto"
+                  whileHover={{ scale: 1.1 }}
+                />
+              </motion.div>
             ))}
           </motion.div>
         </div>
