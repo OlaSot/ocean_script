2f3e00bc866620da306729b046a7073865e0a6ad
Olha Sotova <sotovaola21@gmail.com>
2025-03-12 15:53:03 +0100

    images hav changed

--- next.config.js
+++ next.config.js
@@ -3,11 +3,16 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    domains: ['images.unsplash.com'],
+    formats: ['image/avif', 'image/webp'], 
+    minimumCacheTTL: 60, 
+  },
   reactStrictMode: true,
   experimental: {
     serverActions: {}, 
   },
+  swcMinify: true, 
 };
 
 module.exports = nextConfig;
